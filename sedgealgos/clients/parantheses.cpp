8ac30bf9eb281db0ed56cfbe5e909e0f45140f29
Krzysztof Czapla <k.czapla68@gmail.com>
2022-02-08 23:11:45 +0000

    Make working parantheses client

--- sedgealgos/clients/parantheses.cpp
+++ sedgealgos/clients/parantheses.cpp
@@ -1,3 +1,47 @@
+#include "sedgealgos/io/in.hpp"
+#include "sedgealgos/io/std_out.hpp"
+#include "sedgealgos/stack/resizing_array_stack.hpp"
+
+using namespace sedgealgos::io;
+using namespace sedgealgos::stack;
+
 int main() {
+    In in{};
+    StdOut std_out{};
+
+    std_out.print("input>\n");
+    auto const input{in.reads_all()};
+    
+    ResizingArrayStack<char> stack{};
+    for (auto const c : input) {
+        switch(c) {
+            case '{':
+            case '(':
+            case '[':
+                stack.push(c);
+                break;
+            case '}':
+                if (stack.pop() != '{') {
+                    return 1;
+                }
+                break;
+            case ')':
+                if (stack.pop() != '(') {
+                    return 1;
+                }
+                break;
+            case ']':
+                if (stack.pop() != '[') {
+                    return 1;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    if (!stack.is_empty()) {
+        return 1;
+    }
+    std_out.println("No issues with parantheses.");
     return 0;
 }
