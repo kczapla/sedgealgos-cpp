5aa5830c4b583d477adef5b50240d02e29523e7d
Krzysztof Czapla <k.czapla68@gmail.com>
2022-02-12 22:42:35 +0000

    Add LinkedListStack

---
+++ sedgealgos/stack/linked_list_stack.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+namespace sedgealgos::stack {
+
+template <typename Item>
+class LinkedListStack {
+public:
+  LinkedListStack() = default;
+  LinkedListStack(LinkedListStack const&);
+  LinkedListStack& operator=(LinkedListStack const&);
+  ~LinkedListStack();
+
+  void push(Item item); 
+  Item pop();
+
+  bool is_empty() const;
+  unsigned long size() const;
+
+private:
+  struct Node {
+      Item item;
+      Node* next{nullptr};
+  };
+
+  Node* head{nullptr};
+};
+
+template<typename Item>
+LinkedListStack<Item>::LinkedListStack(LinkedListStack<Item> const& other) {
+  for (auto* i{other.head}; i; i = i->next) {
+    this->push(i->item);
+  }
+}
+
+template<typename Item>
+LinkedListStack<Item>& LinkedListStack<Item>::operator=(LinkedListStack<Item> const& other) {
+  for (auto* i{other.head}; i; i = i->next) {
+    this->push(i->item);
+  }
+
+  return *this;
+}
+
+template<typename Item>
+LinkedListStack<Item>::~LinkedListStack() {
+  while(!is_empty()) {
+    auto* to_delete{head};
+    head = head->next;
+    delete to_delete;
+  }
+}
+
+template<typename Item>
+void LinkedListStack<Item>::push(Item item) {
+  auto* node{new Node};
+  node->item = item;
+  node->next = head;
+  head = node;
+}
+
+template<typename Item>
+Item LinkedListStack<Item>::pop() {
+  auto* node_to_pop{head};
+  head = head->next;
+  auto value_to_pop{node_to_pop->item};
+  delete node_to_pop;
+
+  return value_to_pop;
+}
+
+template<typename Item>
+bool LinkedListStack<Item>::is_empty() const {
+  return !head;
+}
+
+template<typename Item>
+unsigned long LinkedListStack<Item>::size() const {
+  auto count{0};
+  for (auto* i{head}; i; i = i->next) {
+    ++count;
+  }
+  return count;
+}
+}
---
+++ sedgealgos/stack/linked_list_stack_test.cpp
@@ -0,0 +1,10 @@
+#include "sedgealgos/stack/stack_test.hpp"
+#include "sedgealgos/stack/linked_list_stack.hpp"
+
+#include <gtest/gtest.h>
+
+namespace {
+using namespace sedgealgos::stack;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(LinkedList, StackTest, LinkedListStack<int>);
+}
