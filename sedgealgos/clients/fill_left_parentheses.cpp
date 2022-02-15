5a618c532762cd24b412b2f4e179bdcfe8ed5bd0
Krzysztof Czapla <k.czapla68@gmail.com>
2022-02-15 22:51:29 +0000

    Refactor FillLeftParentheses class

--- sedgealgos/clients/fill_left_parentheses.cpp
+++ sedgealgos/clients/fill_left_parentheses.cpp
@@ -1,47 +1,52 @@
-#include "sedgealgos/stack/linked_list_stack.hpp"
 #include "sedgealgos/clients/fill_left_parentheses.hpp"
 
 #include <string>
 
 namespace sedgealgos::clients {
-  std::string FillLeftParentheses::fill(std::string_view statement) const {
-      stack::LinkedListStack<char> operands{};
-      stack::LinkedListStack<char> operators{};
-      stack::LinkedListStack<std::string> expressions{};
-
+  std::string FillLeftParentheses::fill(std::string_view statement) {
       for (auto const c : statement) {
           switch(c) {
               case '/':
               case '*':
               case '-':
               case '+':
-                  operators.push(c);
+                  on_operator_detection(c);
                   break;
               case ')':
-                  {
-                  stack::LinkedListStack<char> reverted_expression{};
-                  reverted_expression.push(')');
-                  while(!operators.is_empty()) {
-                    reverted_expression.push(operands.pop());
-                    reverted_expression.push(operators.pop());
-                  }
-                  reverted_expression.push(operands.pop());
-                  reverted_expression.push('(');
-
-                  std::string new_expression{};
-                  while(!reverted_expression.is_empty()) {
-                      new_expression.push_back(reverted_expression.pop());
-                  }
-
-                  expressions.push(new_expression);
-                  }
+                  on_make_expr_from_operands_and_ops();
                   break;
               default:
-                  operands.push(c);
+                  on_operand_detection(c);
                   break;
           }
       }
 
       return expressions.pop();
+  }
+
+  void FillLeftParentheses::on_operator_detection(char const c) {
+      operators.push(c);
+  }
+
+  void FillLeftParentheses::on_operand_detection(char const c) {
+      operands.push(c);
+  }
+
+  void FillLeftParentheses::on_make_expr_from_operands_and_ops() {
+    stack::LinkedListStack<char> reverted_expression{};
+    reverted_expression.push(')');
+    while(!operators.is_empty()) {
+      reverted_expression.push(operands.pop());
+      reverted_expression.push(operators.pop());
+    }
+    reverted_expression.push(operands.pop());
+    reverted_expression.push('(');
+
+    std::string new_expression{};
+    while(!reverted_expression.is_empty()) {
+        new_expression.push_back(reverted_expression.pop());
+    }
+
+    expressions.push(new_expression);
   }
 }
--- sedgealgos/clients/fill_left_parentheses.hpp
+++ sedgealgos/clients/fill_left_parentheses.hpp
@@ -1,10 +1,21 @@
+#include "sedgealgos/stack/linked_list_stack.hpp"
+
 #include <string>
 #include <string_view>
 
 namespace sedgealgos::clients {
 class FillLeftParentheses {
 public:
-  std::string fill(std::string_view) const;
+  std::string fill(std::string_view);
+
 private:
+  void on_operator_detection(char c);
+  void on_operand_detection(char c);
+
+  void on_make_expr_from_operands_and_ops(); 
+
+  stack::LinkedListStack<char> operands{};
+  stack::LinkedListStack<char> operators{};
+  stack::LinkedListStack<std::string> expressions{};
 };
 }
