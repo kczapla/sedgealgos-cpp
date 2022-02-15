c371053583f4a41ac918d60b91c62b8990f2d6c9
Krzysztof Czapla <k.czapla68@gmail.com>
2022-02-15 22:19:18 +0000

    Refactor test class

--- sedgealgos/clients/fill_left_parentheses_test.cpp
+++ sedgealgos/clients/fill_left_parentheses_test.cpp
@@ -5,28 +5,30 @@
 namespace {
   using namespace ::sedgealgos::clients;
 
-  TEST(FillLeftParenthesesTests, WrapsSingleDigitAdditionOperation) {
-    FillLeftParentheses flp;  
-    EXPECT_EQ(flp.fill("1+2)"), "(1+2)");
+  class FillLeftParenthesesTests : public ::testing::Test {
+  protected:
+      std::string fill(std::string_view s) {
+        return FillLeftParentheses{}.fill(s);
+      }
+  };
+
+  TEST_F(FillLeftParenthesesTests, WrapsSingleDigitAdditionOperation) {
+    EXPECT_EQ(fill("1+2)"), "(1+2)");
   }
 
-  TEST(FillLeftParenthesesTests, AddParenAroundSubtraction) {
-    FillLeftParentheses flp;
-    EXPECT_EQ(flp.fill("1-2)"), "(1-2)");
+  TEST_F(FillLeftParenthesesTests, AddParenAroundSubtraction) {
+    EXPECT_EQ(fill("1-2)"), "(1-2)");
   }
 
-  TEST(FillLeftParenthesesTests, AddParenAroundMultiplication) {
-    FillLeftParentheses flp;
-    EXPECT_EQ(flp.fill("1*2)"), "(1*2)");
+  TEST_F(FillLeftParenthesesTests, AddParenAroundMultiplication) {
+    EXPECT_EQ(fill("1*2)"), "(1*2)");
   }
 
-  TEST(FillLeftParenthesesTests, AddParenAroundDivision) {
-    FillLeftParentheses flp;
-    EXPECT_EQ(flp.fill("1/2)"), "(1/2)");
+  TEST_F(FillLeftParenthesesTests, AddParenAroundDivision) {
+    EXPECT_EQ(fill("1/2)"), "(1/2)");
   }
 
-  TEST(FillLeftParenthesesTests, AddParenAroundCompoundArithmeticOps) {
-    FillLeftParentheses flp;
-    EXPECT_EQ(flp.fill("1/2+3*4-5)"), "(1/2+3*4-5)");
+  TEST_F(FillLeftParenthesesTests, AddParenAroundCompoundArithmeticOps) {
+    EXPECT_EQ(fill("1/2+3*4-5)"), "(1/2+3*4-5)");
   }
 }
