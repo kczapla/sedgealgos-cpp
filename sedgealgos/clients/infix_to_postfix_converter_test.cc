462f47e814e17c8b185d8092c55137fcefad1348
Krzysztof Czapla <k.czapla68@gmail.com>
2022-03-09 00:27:42 +0000

    Refactor unit tests

--- sedgealgos/clients/infix_to_postfix_converter_test.cc
+++ sedgealgos/clients/infix_to_postfix_converter_test.cc
@@ -2,14 +2,31 @@
 
 #include <gtest/gtest.h>
 
+#include <tuple>
+#include <string>
+
 namespace {
 using namespace sedgealgos::clients;
 
-TEST(InfixToPostfixConverterTest, ConverArithmeticOperations) {
-  InfixToPostfixConverter converter;
-  EXPECT_EQ(converter.convert("1+2"), "(1 2 +)");
-  EXPECT_EQ(converter.convert("1-2"), "(1 2 -)");
-  EXPECT_EQ(converter.convert("1*2"), "(1 2 *)");
-  EXPECT_EQ(converter.convert("1/2"), "(1 2 /)");
+using TestCaseData = std::tuple<std::string, std::string>;
+
+class InfixToPostfixConverterTest : public ::testing::TestWithParam<TestCaseData> {
+protected:
+  InfixToPostfixConverter converter{};
+};
+
+TEST_P(InfixToPostfixConverterTest, ConvertArithmeticOperations) {
+  EXPECT_EQ(converter.convert(std::get<0>(GetParam())), std::get<1>(GetParam()));
 }
+
+INSTANTIATE_TEST_CASE_P(
+    ArithemticOps,
+    InfixToPostfixConverterTest,
+    ::testing::Values(
+      std::make_tuple("1+2", "(1 2 +)"),
+      std::make_tuple("1-2", "(1 2 -)"),
+      std::make_tuple("1*2", "(1 2 *)"),
+      std::make_tuple("1/2", "(1 2 /)")
+    )
+);
 }
