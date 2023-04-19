dd33a454fa98361e7cb49f04d501afc72e2134a5
Krzysztof Czapla <k.czapla68@gmail.com>
2023-04-19 20:58:32 +0200

    Random Queue iterator

--- sedgealgos/data_structures/queue/random_queue.hpp
+++ sedgealgos/data_structures/queue/random_queue.hpp
@@ -1,11 +1,87 @@
 #pragma once
 
 #include "sedgealgos/data_structures/array/array.hpp"
+#include "sedgealgos/data_structures/linked_list/single_linked_list.hpp"
+#include "sedgealgos/stack/stack.hpp"
 
 namespace sedgealgos::data_structures::queue {
+
+using LinkedListStack = stack::Stack<unsigned int, linked_list::SingleLinkedList<unsigned int>>;
+
 template<typename Item, typename RandomGenerator>
 class RandomQueue {
 public:
+    template<typename T>
+    class Iterator {
+    public:
+        using value_type = T;
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        struct Begin {};
+        struct End {};
+
+
+        Iterator() = default;
+
+        Iterator(array::Array<value_type>* data, Begin) : data{data} {
+            if (data == nullptr) {
+                return;
+            }
+
+            array::Array<bool> set(data->size(), false);
+
+            auto any_false{[](auto const& set) {
+                for (auto flag : set) {
+                    if (!flag) {
+                        return true;
+                    }
+                }
+                return false;
+            }};
+
+            while(any_false(set)) {
+                auto i{RandomGenerator::uniform(static_cast<int>(data->size() - 1))};
+                if (set[i]) {
+                    continue;
+                }
+                set[i] = true;
+                indices.push(i);
+            }
+        }
+
+        Iterator(array::Array<value_type>* data, End) : data{data} {}
+
+        reference operator*() {
+            return (*data)[indices.peek()];
+        }
+
+        Iterator& operator++() {
+            indices.pop();
+            return *this;
+        }
+
+        bool operator==(Iterator const& rhs) {
+            auto points_to_the_same_data{data == rhs.data};
+            auto have_the_same_indices{indices == rhs.indices};
+            return points_to_the_same_data && have_the_same_indices;
+        }
+
+    private:
+        array::Array<value_type>* data;
+        LinkedListStack indices;
+    };
+
+    using iterator = Iterator<Item>;
+
+    iterator begin() {
+        return iterator{&data, typename iterator::Begin{}};
+    }
+
+    iterator end() {
+        return iterator{&data, typename iterator::End{}};
+    }
+
     bool is_empty() const {
         return data.is_empty();
     }
--- sedgealgos/data_structures/queue/random_queue_test.cpp
+++ sedgealgos/data_structures/queue/random_queue_test.cpp
@@ -90,4 +90,51 @@ TEST(RandomQueueTest, WhenQueueHasTwoItemsThenDequeuReturnsRandomItem) {
 
     EXPECT_TRUE(rq.is_empty());
 }
+
+TEST(RandomQueueTest, BeginIteratorOfEmptyQueueIsEqualToEndIterator) {
+    RandomGeneratorMock::reset_counter();
+
+    GenericRandomIntQueue<RandomGeneratorMock> rq;
+
+    EXPECT_TRUE(rq.begin() == rq.end());
+}
+
+TEST(RandomQueueTest, TwoIteratorsOfTheSameNonEmptyQueueAreNotEqual) {
+    RandomGeneratorMock::reset_counter();
+
+    GenericRandomIntQueue<RandomGeneratorMock> rq;
+
+    rq.enqueue(1);
+    rq.enqueue(2);
+    rq.enqueue(3);
+    rq.enqueue(4);
+
+    auto iter1{rq.begin()};
+    ++iter1;
+    ++iter1;
+
+    RandomGeneratorMock::reset_counter();
+    auto iter2{rq.begin()};
+
+    EXPECT_TRUE(iter1 != iter2);
+}
+
+TEST(RandomQueueTest, IteratingOverRandomQueueReturnsRandomElements) {
+    RandomGeneratorMock::reset_counter();
+
+    GenericRandomIntQueue<RandomGeneratorMock> rq;
+
+    rq.enqueue(1);
+    rq.enqueue(2);
+    rq.enqueue(3);
+    rq.enqueue(4);
+
+    auto iter{rq.begin()};
+    EXPECT_EQ(*iter, 4);
+    EXPECT_EQ(*(++iter), 3);
+    EXPECT_EQ(*(++iter), 2);
+    EXPECT_EQ(*(++iter), 1);
+
+    EXPECT_TRUE(++iter == rq.end());
+}
 }
