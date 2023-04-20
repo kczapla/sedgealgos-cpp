e6ea89a0763d973e838a1a82a9b9078879329aac
Krzysztof Czapla <k.czapla68@gmail.com>
2023-04-20 21:58:26 +0200

    Generalized Queue based on SingleLinkedList

--- sedgealgos/data_structures/queue/linked_list_generalized_queue.hpp
+++ sedgealgos/data_structures/queue/linked_list_generalized_queue.hpp
@@ -1,11 +1,38 @@
 #pragma once
 
+#include "sedgealgos/data_structures/linked_list/single_linked_list.hpp"
+
 namespace sedgealgos::data_structures::queue {
 template <typename Item>
 class LinkedListGeneralizedQueue {
 public:
+    using Size = long unsigned int;
+
     bool is_empty() const {
-        return true;
+        return data.is_empty();
     }
+
+    void insert(Item item) {
+        data.push_front(item);
+    }
+
+    Item remove(Size k) {
+        auto iter{data.begin()};
+        Size index{data.size() - k - 1};
+        Size counter{0};
+        while (counter < index) {
+            ++iter;
+            ++counter;
+        }
+
+        Item val{*iter};
+
+        data.remove(k);
+
+        return val;
+    }
+
+private:
+    linked_list::SingleLinkedList<Item> data;
 };
 }
--- sedgealgos/data_structures/queue/linked_list_generalized_queue_test.cpp
+++ sedgealgos/data_structures/queue/linked_list_generalized_queue_test.cpp
@@ -17,9 +17,89 @@ TYPED_TEST_P(GeneralizedQueueTest, WhenNoItemsInQueueIsEmptyReturnsTrue) {
     EXPECT_TRUE(this->queue.is_empty());
 }
 
+TYPED_TEST_P(GeneralizedQueueTest, WhenItemsInQueueIsEmptyReturnsFalse) {
+    this->queue.insert(1);
+    EXPECT_FALSE(this->queue.is_empty());
+}
+
+TYPED_TEST_P(GeneralizedQueueTest, DeleteItemFromSingleItemQueueMakesItEmpty) {
+    this->queue.insert(1);
+    this->queue.remove(0);
+    EXPECT_TRUE(this->queue.is_empty());
+}
+
+TYPED_TEST_P(GeneralizedQueueTest, RemoveOldestElement) {
+    this->queue.insert(1);
+    this->queue.insert(2);
+    this->queue.insert(3);
+    this->queue.insert(4);
+
+    this->queue.remove(0);
+
+    EXPECT_EQ(this->queue.remove(0), 1);
+}
+
+TYPED_TEST_P(GeneralizedQueueTest, RemoveLatestElement) {
+    this->queue.insert(1);
+    this->queue.insert(2);
+    this->queue.insert(3);
+    this->queue.insert(4);
+
+    EXPECT_EQ(this->queue.remove(3), 4);
+}
+
+TYPED_TEST_P(GeneralizedQueueTest, RemoveElementFromTheMiddle) {
+    this->queue.insert(1);
+    this->queue.insert(2);
+    this->queue.insert(3);
+    this->queue.insert(4);
+
+    EXPECT_EQ(this->queue.remove(1), 2);
+}
+
+TYPED_TEST_P(GeneralizedQueueTest, RemoveAllItemsFromQueue) {
+    this->queue.insert(1);
+    this->queue.insert(2);
+    this->queue.insert(3);
+    this->queue.insert(4);
+
+    this->queue.remove(0);
+    this->queue.remove(0);
+    this->queue.remove(0);
+    this->queue.remove(0);
+
+    EXPECT_TRUE(this->queue.is_empty());
+}
+
+TYPED_TEST_P(GeneralizedQueueTest, AddItemsToQueueFromWhichAllItemsWereDeleted) {
+    this->queue.insert(1);
+    this->queue.insert(2);
+    this->queue.insert(3);
+    this->queue.insert(4);
+
+    this->queue.remove(0);
+    this->queue.remove(0);
+    this->queue.remove(0);
+    this->queue.remove(0);
+
+    this->queue.insert(5);
+    this->queue.insert(6);
+    this->queue.insert(7);
+    this->queue.insert(8);
+
+    EXPECT_EQ(this->queue.remove(1), 6);
+}
+
 REGISTER_TYPED_TEST_SUITE_P(
         GeneralizedQueueTest,
-        WhenNoItemsInQueueIsEmptyReturnsTrue
+        WhenNoItemsInQueueIsEmptyReturnsTrue,
+        WhenItemsInQueueIsEmptyReturnsFalse,
+        DeleteItemFromSingleItemQueueMakesItEmpty,
+        RemoveOldestElement,
+        RemoveLatestElement,
+        RemoveElementFromTheMiddle,
+        RemoveAllItemsFromQueue,
+        AddItemsToQueueFromWhichAllItemsWereDeleted
 );
 
 INSTANTIATE_TYPED_TEST_SUITE_P(LinkedList, GeneralizedQueueTest, sedgealgos::data_structures::queue::LinkedListGeneralizedQueue<int>);
