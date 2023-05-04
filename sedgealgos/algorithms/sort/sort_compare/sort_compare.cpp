fc02e8f1d916045f7d5dda3c1992bef21e9f949c
Krzysztof Czapla <k.czapla68@gmail.com>
2023-05-04 22:41:26 +0200

    Real sort compare

---
+++ sedgealgos/algorithms/sort/sort_compare/sort_compare.cpp
@@ -0,0 +1,47 @@
+#include "sedgealgos/io/std_out.hpp"
+#include "sedgealgos/data_structures/array/array.hpp"
+
+#include <string>
+
+using String = std::string;
+using IntArray = sedgealgos::data_structures::array::Array<int>;
+
+double time(String alg, IntArray const& arr) {
+    return 0;
+}
+
+double timeRandomInput(String alg, int n, int tries) {
+    double total{0};
+    IntArray ia;
+    
+    for (int t{0}; t < tries; t++) {
+        for (int i{0}; i < n; i++) {
+            ia.push_back(i);
+        }
+
+        total += time(alg, ia);
+    }
+
+    return total;
+}
+
+int main(int argc, char** argv) {
+    sedgealgos::io::StdOut std_out;
+    if (argc != 5) {
+        std_out.println("please provide two cmdline args");
+        return 1;
+    }
+
+    String alg1{argv[1]};
+    String alg2{argv[2]};
+
+    auto n{std::stoi(argv[3])};
+    auto t{std::stoi(argv[4])};
+
+    auto t1{timeRandomInput(alg1, n, t)};
+    auto t2{timeRandomInput(alg2, n, t)};
+
+    std_out.printf("%s is faster '%d' times than %s\n", alg1, t1/t2, alg2);
+
+    return 0;
+}
