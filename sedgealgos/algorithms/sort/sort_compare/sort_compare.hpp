c98e1f9940c9784839e1e504d26c8d7debc0918a
Krzysztof Czapla <k.czapla68@gmail.com>
2023-05-04 22:42:04 +0200

    Remove redundat files

--- sedgealgos/algorithms/sort/sort_compare/sort_compare.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace sedgealgos::algorithms::sort::sort_compare {
-class SortCompare {
-};
-}
-
--- sedgealgos/algorithms/sort/sort_compare/sort_compare_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "sedgealgos/algorithms/sort/sort_compare/sort_compare.hpp"
-
-#include <gtest/gtest.h>
-
-namespace {
-TEST(SortCompareTest, TestCase1) {
-}
-}
-
