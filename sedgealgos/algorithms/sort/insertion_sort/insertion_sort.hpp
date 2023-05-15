4bef5da9fd7a28dbc003e5ea16277215b49c4e57
Krzysztof Czapla <k.czapla68@gmail.com>
2023-05-15 23:13:29 +0200

    small modification of insertion sort

--- sedgealgos/algorithms/sort/insertion_sort/insertion_sort.hpp
+++ sedgealgos/algorithms/sort/insertion_sort/insertion_sort.hpp
@@ -7,16 +7,12 @@ namespace sedgealgos::algorithms::sort::insertion_sort {
 template <typename T>
 void sort(sedgealgos::data_structures::array::Array<T>& arr) {
     for (long unsigned int i{1}; i < arr.size(); i++) {
-        for (long unsigned int j{i}; j > 0; j--) {
-            if (arr[j-1] > arr[j]) {
+        for (long unsigned int j{i}; j > 0 && arr[j] < arr[j-1]; j--) {
                 auto tmp{arr[j]};
                 arr[j] = arr[j-1];
                 arr[j-1] = tmp;
-            } else {
-                j = 1;
             }
         } 
     }
-}
 }
 
