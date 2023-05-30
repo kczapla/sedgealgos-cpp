a0eaad4bd5276b38a0fada88c7f3c816170fcdec
Krzysztof Czapla <k.czapla68@gmail.com>
2023-05-30 06:59:51 +0200

    refactor sorting tests

--- sedgealgos/algorithms/sort/insertion_sort/sort_test.cpp
+++ sedgealgos/algorithms/sort/insertion_sort/sort_test.cpp
@@ -24,124 +24,85 @@ using SortingFunc = std::function<void(Array<int>&)>;
 struct TestParam {
     Array<int> to_sort;
     Array<int> sorted;
-    SortingFunc sort;
 };
 
 struct SortTest : public ::testing::TestWithParam<TestParam> {};
 
-TEST_P(SortTest, SortArray) {
-    auto [to_sort, sorted, sort]{GetParam()};
-    sort(to_sort);
+TEST_P(SortTest, InsertionSort) {
+    auto [to_sort, sorted]{GetParam()};
+
+    insertion_sort::sort(to_sort);
+
     EXPECT_EQ(to_sort, sorted);
 }
 
-TestParam make_insertion_sort_test_param(Array<int> const& to_sort, Array<int> const& sorted) {
-    return TestParam{to_sort, sorted, insertion_sort::sort<int>};
+TEST_P(SortTest, SelectionSort) {
+    auto [to_sort, sorted]{GetParam()};
+
+    selection_sort::sort(to_sort);
+
+    EXPECT_EQ(to_sort, sorted);
 }
 
-TestParam make_selection_sort_test_param(Array<int> const& to_sort, Array<int> const& sorted) {
-    return TestParam{to_sort, sorted, selection_sort::sort<int>};
+TEST_P(SortTest, ShellSort) {
+    auto [to_sort, sorted]{GetParam()};
+
+    shell_sort::sort(to_sort);
+
+    EXPECT_EQ(to_sort, sorted);
 }
 
-TestParam make_shell_sort_test_param(Array<int> const& to_sort, Array<int> const& sorted) {
-    return TestParam{to_sort, sorted, shell_sort::sort<int>};
+TEST_P(SortTest, ShellSelectionSort) {
+    auto [to_sort, sorted]{GetParam()};
+
+    shell_selection_sort::sort(to_sort);
+
+    EXPECT_EQ(to_sort, sorted);
 }
 
-TestParam make_shell_selection_sort_test_param(Array<int> const& to_sort, Array<int> const& sorted) {
-    return TestParam{to_sort, sorted, shell_selection_sort::sort<int>};
+TEST_P(SortTest, ShellPrecalcGapsSort) {
+    auto [to_sort, sorted]{GetParam()};
+
+    shell_precalc_gaps_sort::sort(to_sort);
+
+    EXPECT_EQ(to_sort, sorted);
 }
 
-TestParam make_shell_precalc_gaps_sort_test_param(Array<int> const& to_sort, Array<int> const& sorted) {
-    return TestParam{to_sort, sorted, shell_precalc_gaps_sort::sort<int>};
+TEST_P(SortTest, InsertionSortWithSentry) {
+    auto [to_sort, sorted]{GetParam()};
+
+    insertion_sort_with_sentry::sort(to_sort);
+
+    EXPECT_EQ(to_sort, sorted);
 }
 
-TestParam make_insertion_sort_with_sentry(Array<int> const& to_sort, Array<int> const& sorted) {
-    return TestParam{to_sort, sorted, insertion_sort_with_sentry::sort<int>};
+TEST_P(SortTest, InsertionSortWithoutExchange) {
+    auto [to_sort, sorted]{GetParam()};
+
+    insertion_sort_without_exchange::sort(to_sort);
+
+    EXPECT_EQ(to_sort, sorted);
 }
 
-TestParam make_insertion_sort_without_exchange(Array<int> const& to_sort, Array<int> const& sorted) {
-    return TestParam{to_sort, sorted, insertion_sort_without_exchange::sort<int>};
-}
+TEST_P(SortTest, TopDownMergeSort) {
+    GTEST_SKIP();
+    auto [to_sort, sorted]{GetParam()};
 
-INSTANTIATE_TEST_SUITE_P(InsertionSort, SortTest, testing::Values(
-    make_insertion_sort_test_param({}, {}),
-    make_insertion_sort_test_param({1}, {1}),
-    make_insertion_sort_test_param({1, 2}, {1, 2}),
-    make_insertion_sort_test_param({2, 1}, {1, 2}),
-    make_insertion_sort_test_param({2, 1, 3}, {1, 2, 3}),
-    make_insertion_sort_test_param({2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}),
-    make_insertion_sort_test_param({2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}),
-    make_insertion_sort_test_param({2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187})
-));
-
-INSTANTIATE_TEST_SUITE_P(SelectionSort, SortTest, testing::Values(
-    make_selection_sort_test_param({}, {}),
-    make_selection_sort_test_param({1}, {1}),
-    make_selection_sort_test_param({1, 2}, {1, 2}),
-    make_selection_sort_test_param({2, 1}, {1, 2}),
-    make_selection_sort_test_param({2, 1, 3}, {1, 2, 3}),
-    make_selection_sort_test_param({2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}),
-    make_selection_sort_test_param({2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}),
-    make_selection_sort_test_param({2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187})
-));
-
-INSTANTIATE_TEST_SUITE_P(ShellSort, SortTest, testing::Values(
-    make_shell_sort_test_param({}, {}),
-    make_shell_sort_test_param({1}, {1}),
-    make_shell_sort_test_param({1, 2}, {1, 2}),
-    make_shell_sort_test_param({2, 1}, {1, 2}),
-    make_shell_sort_test_param({2, 1, 3}, {1, 2, 3}),
-    make_shell_sort_test_param({2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}),
-    make_shell_sort_test_param({2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}),
-    make_shell_sort_test_param({2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187})
-));
-
-INSTANTIATE_TEST_SUITE_P(ShellSelectionSort, SortTest, testing::Values(
-    make_shell_selection_sort_test_param({}, {}),
-    make_shell_selection_sort_test_param({1}, {1}),
-    make_shell_selection_sort_test_param({1, 2}, {1, 2}),
-    make_shell_selection_sort_test_param({2, 1}, {1, 2}),
-    make_shell_selection_sort_test_param({2, 1, 3}, {1, 2, 3}),
-    make_shell_selection_sort_test_param({2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}),
-    make_shell_selection_sort_test_param({2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}),
-    make_shell_selection_sort_test_param({2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187})
-));
-
-INSTANTIATE_TEST_SUITE_P(ShellPrecalcGapsSort, SortTest, testing::Values(
-    make_shell_precalc_gaps_sort_test_param({}, {}),
-    make_shell_precalc_gaps_sort_test_param({1}, {1}),
-    make_shell_precalc_gaps_sort_test_param({1, 2}, {1, 2}),
-    make_shell_precalc_gaps_sort_test_param({2, 1}, {1, 2}),
-    make_shell_precalc_gaps_sort_test_param({2, 1, 3}, {1, 2, 3}),
-    make_shell_precalc_gaps_sort_test_param({2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}),
-    make_shell_precalc_gaps_sort_test_param({2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}),
-    make_shell_precalc_gaps_sort_test_param({2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187})
-));
-
-INSTANTIATE_TEST_SUITE_P(InsertionSortWithSentry, SortTest, testing::Values(
-    make_insertion_sort_with_sentry({}, {}),
-    make_insertion_sort_with_sentry({1}, {1}),
-    make_insertion_sort_with_sentry({1, 2}, {1, 2}),
-    make_insertion_sort_with_sentry({2, 1}, {1, 2}),
-    make_insertion_sort_with_sentry({2, 1, 3}, {1, 2, 3}),
-    make_insertion_sort_with_sentry({2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}),
-    make_insertion_sort_with_sentry({2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}),
-    make_insertion_sort_with_sentry({2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187})
-));
-
-INSTANTIATE_TEST_SUITE_P(InsertionSortWithoutExchange, SortTest, testing::Values(
-    make_insertion_sort_without_exchange({}, {}),
-    make_insertion_sort_without_exchange({1}, {1}),
-    make_insertion_sort_without_exchange({1, 2}, {1, 2}),
-    make_insertion_sort_without_exchange({2, 1}, {1, 2}),
-    make_insertion_sort_without_exchange({2, 1, 3}, {1, 2, 3}),
-    make_insertion_sort_without_exchange({2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}),
-    make_insertion_sort_without_exchange({2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}),
-    make_insertion_sort_without_exchange({2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187})
-));
-
-TEST(TopDownMergeSort, Test) {
     sort::CallbacksMock cm;
     merge_sort::top_down::Sort ms{&cm};
+
+    EXPECT_EQ(to_sort, sorted);
 }
+
+
+INSTANTIATE_TEST_SUITE_P(Sort, SortTest, testing::Values(
+    TestParam{{}, {}},
+    TestParam{{1}, {1}},
+    TestParam{{1, 2}, {1, 2}},
+    TestParam{{2, 1}, {1, 2}},
+    TestParam{{2, 1, 3}, {1, 2, 3}},
+    TestParam{{2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 3, 30, 39, 40, 187}},
+    TestParam{{2, 1, 3, -1, 40, 39, 3, 30, 187}, {-1, 1, 2, 3, 3, 30, 39, 40, 187}},
+    TestParam{{2, 2, 1, 3, -1, 40, 39, 30, 187}, {-1, 1, 2, 2, 3, 30, 39, 40, 187}}
+));
 }
