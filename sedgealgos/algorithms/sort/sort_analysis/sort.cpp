d71cc24dc5676b0df1b5eb443a391d07476ad801
Krzysztof Czapla <k.czapla68@gmail.com>
2023-05-31 07:48:12 +0200

    run merge sort

--- sedgealgos/algorithms/sort/sort_analysis/sort.cpp
+++ sedgealgos/algorithms/sort/sort_analysis/sort.cpp
@@ -1,13 +1,18 @@
 // #include "sedgealgos/algorithms/sort/merge_sort/bottom_up/sort.hpp"
 #include "sedgealgos/algorithms/sort/merge_sort/top_down/sort.hpp"
+#include "sedgealgos/algorithms/sort/sort/callbacks/sort_statistics.hpp"
 #include "sedgealgos/algorithms/sort/sort_analysis/generate_array.hpp"
 #include "sedgealgos/data_structures/string/string.hpp"
 #include "sedgealgos/io/std_out.hpp"
 
 sedgealgos::data_structures::string::String run(auto alg, auto distribution, auto n) {
     auto arr{sedgealgos::algorithms::sort::sort_analysis::generate_array<int>(n, distribution)};
+    sedgealgos::algorithms::sort::sort::callbacks::SortStatistics stats_callbacks;
+    sedgealgos::algorithms::sort::merge_sort::top_down::Sort top_down_merge_sort{&stats_callbacks};
 
-    return "";
+    top_down_merge_sort.sort(arr);
+
+    return stats_callbacks.to_json();
 }
 
 int main(int argc, char** argv) {
