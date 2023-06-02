d6cdabb5dd0b4689a2f069a665ea85f561a3babb
Krzysztof Czapla <k.czapla68@gmail.com>
2023-06-02 09:12:11 +0200

    Faster merge is not working

--- sedgealgos/algorithms/sort/merge_sort/merge_sort_test.cpp
+++ sedgealgos/algorithms/sort/merge_sort/merge_sort_test.cpp
@@ -1,6 +1,7 @@
 #include "sedgealgos/algorithms/sort/merge_sort/top_down/sort.hpp"
 #include "sedgealgos/algorithms/sort/sort/callbacks_mock.hpp"
 #include "sedgealgos/algorithms/sort/merge_sort/merge/merge.hpp"
+#include "sedgealgos/algorithms/sort/merge_sort/merge/faster_merge.hpp"
 
 #include "sedgealgos/data_structures/array/array.hpp"
 
@@ -20,22 +21,37 @@ struct TestParam {
     Container to_sort;
 };
 
-struct MergeSortTest : public ::testing::TestWithParam<TestParam> {};
+struct TopDownMergeSortTest : public ::testing::TestWithParam<TestParam> {
+protected:
+    TopDownMergeSortTest() {
+        EXPECT_CALL(callbacks, on_array_access).Times(::testing::AnyNumber());
+        EXPECT_CALL(callbacks, on_sort_start).Times(::testing::AnyNumber());
+        EXPECT_CALL(callbacks, on_sort_stop).Times(::testing::AnyNumber());
+    }
 
-TEST_P(MergeSortTest, TopDownMergeSortWithStandardMerge) {
+    sort::CallbacksMock callbacks;
+};
+
+TEST_P(TopDownMergeSortTest, StandardMerge) {
     auto [to_sort]{GetParam()};
 
-    sort::CallbacksMock cm;
-    EXPECT_CALL(cm, on_array_access).Times(::testing::AnyNumber());
-    EXPECT_CALL(cm, on_sort_start).Times(::testing::AnyNumber());
-    EXPECT_CALL(cm, on_sort_stop).Times(::testing::AnyNumber());
-    
-    merge_sort::top_down::Sort<Container> ms{merge_sort::merge::merge<Container>, &cm};
+    merge_sort::top_down::Sort<Container> ms{merge_sort::merge::merge<Container>, &callbacks};
 
     ms.sort(to_sort);
 
     EXPECT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
 }
+
+TEST_P(TopDownMergeSortTest, FasterMerge) {
+    auto [to_sort]{GetParam()};
+
+    merge_sort::top_down::Sort<Container> ms{merge_sort::merge::faster_merge<Container>, &callbacks};
+
+    ms.sort(to_sort);
+
+    EXPECT_TRUE(std::is_sorted(to_sort.begin(), to_sort.end()));
+}
+
 
 Container generate_test_container(typename Container::Size size) {
     std::random_device rd;
@@ -50,5 +66,13 @@ Container generate_test_container(typename Container::Size size) {
     return container;
 }
 
-INSTANTIATE_TEST_SUITE_P(StandardMerge, MergeSortTest, testing::Values(TestParam{generate_test_container(0)}));
+INSTANTIATE_TEST_SUITE_P(MergeSort, TopDownMergeSortTest, testing::Values(
+    TestParam{generate_test_container(0)},
+    TestParam{generate_test_container(1)},
+    TestParam{generate_test_container(2)},
+    TestParam{generate_test_container(4)},
+    TestParam{generate_test_container(10)},
+    TestParam{generate_test_container(32)},
+    TestParam{generate_test_container(1024)}
+    ));
 }
