153210200b5b8e1869bafa08b28be213ac71c65e
Krzysztof Czapla <k.czapla68@gmail.com>
2023-09-30 23:57:18 +0200

    Add binary heap priority queue

--- sedgealgos/data_structures/priority_queue/binary_heap_priority_queue.hpp
+++ sedgealgos/data_structures/priority_queue/binary_heap_priority_queue.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include "sedgealgos/data_structures/array/array.hpp"
 
 namespace sedgealgos::data_structures::priority_queue {
@@ -8,7 +9,7 @@ class BinaryHeapPriorityQueue {
 public:
 	using container = Container;
 
-	BinaryHeapPriorityQueue(unsigned int size) : heap_size{0}, heap_capacity{size}, c(size) {};
+	BinaryHeapPriorityQueue(unsigned int size) : heap_size{0}, heap_capacity{size}, c(size+1) {};
 
 	BinaryHeapPriorityQueue(Container container)
 		: BinaryHeapPriorityQueue(container.size() + static_cast<Container::Size>(1)) {
@@ -26,12 +27,19 @@ public:
 	T const& top() const { return this->c[1]; };
 
 	T pop() { 
-		return this->c[heap_size--];
-	};
+		std::swap(c[1], c[heap_size]);
 
-	bool empty() { return false; }
+		auto const top{this->c[heap_size--]};
+		sink(1);
 
-	unsigned int size() const { return 0; };
+		return top;
+	}
+
+	bool empty() { return heap_size == 0; }
+
+	unsigned int size() const { 
+		return heap_size;
+	}
 
 private:
 	unsigned long heap_size;
@@ -41,8 +49,21 @@ private:
 	void swim(unsigned long i) {
 		while(i > 1) {
 			auto const parent{i/2};
+			if (c[i] < this->c[parent]) break;
 			std::swap(c[parent], c[i]);
 			i = parent;
+		}
+	}
+
+	void sink(unsigned long i) {
+		while(i * 2 <= this->heap_size) {
+			auto j{i * 2};
+			
+			if (j+1 <= this->heap_size && this->c[j] < this->c[j+1]) ++j;
+
+			std::swap(c[i], c[j]);
+
+			i = j;
 		}
 	}
 };
--- sedgealgos/data_structures/priority_queue/priority_queue_test.cpp
+++ sedgealgos/data_structures/priority_queue/priority_queue_test.cpp
@@ -11,10 +11,84 @@ namespace {
 using MaxPriorityQueue = MaxBinaryHeapPriorityQueue<int>;
 
 TEST(MaxPriorityQueueTest, MakePriorityListFromArray) {
-	MaxPriorityQueue mpq{Array<int>{1, 2}};
+	MaxPriorityQueue mpq{Array<int>{3, 2, 9, 5, 4, 1, 0, 7, 6, 0}};
 
+	EXPECT_EQ(mpq.pop(), 9);
+	EXPECT_EQ(mpq.pop(), 7);
+	EXPECT_EQ(mpq.pop(), 6);
+	EXPECT_EQ(mpq.pop(), 5);
+	EXPECT_EQ(mpq.pop(), 4);
+	EXPECT_EQ(mpq.pop(), 3);
 	EXPECT_EQ(mpq.pop(), 2);
 	EXPECT_EQ(mpq.pop(), 1);
+	EXPECT_EQ(mpq.pop(), 0);
+	EXPECT_EQ(mpq.pop(), 0);
+}
+
+TEST(MaxPriorityQueueTest, MakeOneElementPriorityQueue) {
+	MaxPriorityQueue mpq{Array<int>{3}};
+
+	EXPECT_EQ(mpq.size(), 1);
+	EXPECT_FALSE(mpq.empty());
+
+	EXPECT_EQ(mpq.pop(), 3);
+	EXPECT_EQ(mpq.size(), 0);
+	EXPECT_TRUE(mpq.empty());
+}
+
+TEST(MaxPriorityQueueTest, MakePriorityListFromInserts) {
+	MaxPriorityQueue mpq(10);
+
+	mpq.insert(3);
+	EXPECT_EQ(mpq.top(), 3);
+	EXPECT_EQ(mpq.size(), 1);
+
+	mpq.insert(2);
+	EXPECT_EQ(mpq.top(), 3);
+	EXPECT_EQ(mpq.size(), 2);
+
+	mpq.insert(9);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 3);
+
+	mpq.insert(5);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 4);
+
+	mpq.insert(4);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 5);
+
+	mpq.insert(1);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 6);
+
+	mpq.insert(0);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 7);
+
+	mpq.insert(7);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 8);
+
+	mpq.insert(6);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 9);
+
+	mpq.insert(0);
+	EXPECT_EQ(mpq.top(), 9);
+	EXPECT_EQ(mpq.size(), 10);
+
+	EXPECT_EQ(mpq.pop(), 9);
+	EXPECT_EQ(mpq.pop(), 7);
+	EXPECT_EQ(mpq.pop(), 6);
+	EXPECT_EQ(mpq.pop(), 5);
+	EXPECT_EQ(mpq.pop(), 4);
+	EXPECT_EQ(mpq.pop(), 3);
+	EXPECT_EQ(mpq.pop(), 2);
+	EXPECT_EQ(mpq.pop(), 1);
+	EXPECT_EQ(mpq.pop(), 0);
+	EXPECT_EQ(mpq.pop(), 0);
 }
 }
 
