91c04bcd04cbe1749b57a4e7b9a1eb389e0cdb52
Krzysztof Czapla <k.czapla68@gmail.com>
2023-10-17 10:24:01 +0200

    Fix Array dtor

--- sedgealgos/data_structures/array/array.hpp
+++ sedgealgos/data_structures/array/array.hpp
@@ -210,7 +210,7 @@ private:
         for (Size i{0}; i < next_index; ++i) {
             new_data[i] = data[i];
         }
-        delete data;
+        delete[] data;
         data = new_data;
 
         capacity = new_capacity;
